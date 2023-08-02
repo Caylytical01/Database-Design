d64b2e9d988e8ae1c72a49a0bcca9a7cb0e18f19
Kiauna N <17782912+Caylytical01@users.noreply.github.com>
2023-08-02 14:42:27 -0500

    Add files via upload

---
+++ Week4.sql
@@ -0,0 +1,14 @@
+insert into categories 
+	(category_name)
+values 
+	('Brass');
+    
+update categories 
+set category_name= 'Woodwinds'
+where category_id = 5;
+
+delete from categories
+where category_id = 5;
+
+
+    
